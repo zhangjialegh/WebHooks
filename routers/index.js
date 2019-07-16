3a12778babaaa73945588e0d5508246db84551ff
zhangjialegh <zjl1195185765@outlook.com>
2019-07-16 22:04:19 +0800

    modify post

--- routers/index.js
+++ routers/index.js
@@ -4,6 +4,6 @@ const router = new Router({
     prefix: '/webhooks'
 })
 
-router.get('/publish', controller.GitlabPublish)
+router.post('/publish', controller.GitlabPublish)
 
 module.exports = router
